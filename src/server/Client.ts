af5346939ab333c914fd6f201556a07f57572a1e
조민제 <chominje@jominje-ui-MacBookAir.local>
2021-06-12 11:57:41 +0900

    연결 끊긴 클라이언트 헨들링

--- src/server/Client.ts
+++ src/server/Client.ts
@@ -11,22 +11,14 @@ class Client {
     public setClientInfo(socket: Socket, room: string) {
         socket.on('client_setClientInfo', (clientIP) =>{
           this.clientInfo = {socketID: socket.id, clientIP: clientIP};
-          socket.to(room).emit("dashBoard_setClientInfo", this.clientInfo);
-          // console.log(clientObject.dashSocketID);
-          
-          // if(!clientObject.dashSocketID){
-          //   socket.to(room).emit("dashBoard_setClientInfo", this.clientInfo);
-          // }
-          // else{
-          //   socket.to(room).to(clientObject.dashSocketID).emit("dashBoard_setClientInfo", this.clientInfo);
-          // }
+          socket.to(room).emit("dashBoard_setClientInfo", );
         })
       }
     // 연결 끊김
     public disconnect(socket: Socket, room: string) {
       socket.on("disconnect", ()=>{
           console.log(socket.id + ": 연결 해제");
-          socket.to(room).emit("dashBoard_clientDisconnect", this.clientInfo);
+          socket.to(room).emit("dashBoard_clientDisconnect", socket.id);
       })
     }
   }
